e0e35b5f4bc2254a35c40e2b8ef4437fc1bfdb7d
Yazan Tarifi <yazantarifi98@gmail.com>
2022-04-01 15:24:34 +0300

    Generate Single Module Application

---
+++ projects/AndroidSingleModular/app/src/main/java/com/yazantarifi/legora/screens/ApplicationLinkScreen.kt
@@ -0,0 +1,14 @@
+package com.yazantarifi.legora.screens
+
+import android.os.Bundle
+import androidx.appcompat.app.AppCompatActivity
+import com.yazantarifi.legora.R
+
+class ApplicationLinkScreen: AppCompatActivity() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+    }
+
+}
