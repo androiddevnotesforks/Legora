50c1f06ee25f8d15f1857aa151095aea31d96aff
Yazan Tarifi <yazantarifi98@gmail.com>
2022-04-01 14:31:53 +0300

    Disable Dependencies Screen, Generate Android Multi Modular Appliication

--- src/main/kotlin/models/ApplicationRoute.kt
+++ src/main/kotlin/models/ApplicationRoute.kt
@@ -1,5 +1,9 @@
 package models
 
+/**
+ * Application Dependencies is Disabled
+ * Will be Enabled in Version 2.0
+ */
 sealed class ApplicationRoute(var route: Int, var prevRoute: Int, var nextRoute: Int) {
 
     companion object {
@@ -15,7 +19,7 @@ sealed class ApplicationRoute(var route: Int, var prevRoute: Int, var nextRoute:
 
     class SplashScreenRouter: ApplicationRoute(SPLASH_SCREEN_ROUTE, NO_SCREEN_POSITION, MAIN_SCREEN_ROUTE)
     class MainScreenRouter: ApplicationRoute(MAIN_SCREEN_ROUTE, NO_SCREEN_POSITION, APP_INFO_SCREEN_ROUTE)
-    class ApplicationInfoRouter: ApplicationRoute(APP_INFO_SCREEN_ROUTE, MAIN_SCREEN_ROUTE, DEPENDENCIES_SCREEN_ROUTE)
+    class ApplicationInfoRouter: ApplicationRoute(APP_INFO_SCREEN_ROUTE, MAIN_SCREEN_ROUTE, APP_PATH_GENERATED_PICKER)
     class ApplicationDependenciesRouter: ApplicationRoute(DEPENDENCIES_SCREEN_ROUTE, APP_INFO_SCREEN_ROUTE, APP_PATH_GENERATED_PICKER)
     class ApplicationPathPickerRouter: ApplicationRoute(APP_PATH_GENERATED_PICKER, DEPENDENCIES_SCREEN_ROUTE, GENERATE_APP_CLASSES)
     class ApplicationGeneratorRouter: ApplicationRoute(GENERATE_APP_CLASSES, NO_SCREEN_POSITION, SUCCESS_SCREEN_ROUTE)
