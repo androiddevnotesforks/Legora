e52d69abc80026806d2a476f00c6d1a7d7b1106e
Yazan Tarifi <yazantarifi98@gmail.com>
2022-05-04 15:28:41 -0700

    Fix Configuration of the Core Javascript Code

---
+++ core/index.js
@@ -0,0 +1,12 @@
+/*
+ * This File is the Primary File in Legora Core Source Code
+ * From Here the All Code for Modules, Generators, Projects, Attributes will be in This Project
+ * This Project Will be Shared with Website, CLI, and The Desktop Application Will Use This Attributes
+ * Return values from Website Server Request and Draw the UI on Desktop, Then Submit the Results to the Generator CLI
+ * By Executing The Command Line CLI Arguments for Each Generator in the Project
+ *
+ * Project Author: Yazan Tarifi : https://github.com/Yazan98
+ * Project Link : https://github.com/Yazan98/Legora
+ * Project Website https://legora.me
+ *
+ */
